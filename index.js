999aad2d1b614d2e48c7f4615a8e15517a6d9590
devgenx <sebastian110420@gmail.com>
2022-12-12 11:04:59 -0500

    commit tree 69cc24ee21a6c86804b12e50e6f939f6971fcee6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+const saturdayFun = (arg = "roller-skate") => {
+  return `This Saturday, I want to ${arg}!`;
+};
+
+const mondayWork = (arg = "go to the office") => {
+  return `This Monday, I will ${arg}.`;
+};
+
+const wrapAdjective = (str = "*") => {
+  return (arg = "special") => {
+    return `You are ${str}${arg}${str}!`;
+  };
+};
